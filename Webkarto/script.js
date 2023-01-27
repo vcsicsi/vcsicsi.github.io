643bf638dbcedf75d5da0563f76d1fe51c6582d5
Vincze Csilla <vcsicsi222@gmail.com>
2023-01-27 16:31:30 +0100

    initial commit

--- Webkarto/script.js
+++ Webkarto/script.js
@@ -23,9 +23,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         'View <a href="https://github.com/HandsOnDataViz/leaflet-map-csv" target="_blank">map-csv code on GitHub</a>'
                     );  
 
+var tooltip=[];                    
 //stations layergroup
-//"https://odp.met.hu/climate/observations_hungary/10_minutes/station_meta_auto.csv"   
-$.get('./data.csv', function(csv) {
+let url = "http://terkeptar.elte.hu/~saman/get.php?url=https://odp.met.hu/climate/observations_hungary/10_minutes/station_meta_auto.csv"   
+$.get(url/*-'./data.csv'*/, function(csv) {
 var data = Papa.parse(csv, {header: true, download: false, encoding: "UTF-8", delimiter: ";", 
             skipEmptyLines: true, dynamicTyping: true, 
             transformHeader:function(h) {
@@ -41,7 +42,8 @@ var data = Papa.parse(csv, {header: true, download: false, encoding: "UTF-8", de
                 stations_layer = L.geoJSON(feature, {
                                 pointToLayer: function (feature, latlng) {
                                             return L.marker(latlng, {icon: stationIcon});
-                                    }}).bindTooltip(row.StationNumber + ", "+ row.RegioName + "," + row.StationName).addTo(stations)
+                                    }}).bindTooltip(row.StationNumber + ", "+ row.RegioName + "," + row.StationName).addTo(stations);
+                                    tooltip.push(row.StationNumber+","+row.StationName);
                 stationsGeoJSON.features.push(feature)
                 })    
             }
@@ -135,52 +137,102 @@ map.on('click', function(e) {
 //https://leafletjs.com/examples/mobile/                                                             
 
 //----------------------------------------------Graph --------------------------------------------------------------   
-//function statkeres() {};
-// metdata fom zip file
-var zip = new JSZip();
-const datum=[];
-const akthom=[];
-let url= 'http://terkeptar.elte.hu/~saman/get.php?url=https://odp.met.hu/climate/observations_hungary/10_minutes/now/HABP_10M_23703_now.zip';    
-fetch(url/*'./Webkarto/HABP_10M_13704_now.zip'*/)
-        .then(r=>r.arrayBuffer())
-            .then(d=>zip.loadAsync(d))
-                .then(z=>z.file(/./)[0].async("text"))
-                    .then(d=>{
-                        let sorok=d.split('\n');
-                        for (let i=7;i<sorok.length;i++) {
-                            let adatok=sorok[i].split(';');
-                            for (let j in adatok) {
-                                adatok[j]=adatok[j].trim();
+function statkeres() {
+    // metdata fom zip file
+    var code=document.getElementById("code").value;
+    if (code.length<1) code=44527;
+    var zip = new JSZip();
+    const datum=[];
+    const akthom=[];
+    const akthoms=[];
+    let url= 'http://terkeptar.elte.hu/~saman/get.php?url=https://odp.met.hu/climate/observations_hungary/10_minutes/now/HABP_10M_'+code+'_now.zip';    
+    fetch(url/*'./Webkarto/HABP_10M_13704_now.zip'*/)
+            .then(r=>r.arrayBuffer())
+                .then(d=>zip.loadAsync(d))
+                    .then(z=>z.file(/./)[0].async("text"))
+                        .then(d=>{
+                            let sorok=d.split('\n');
+                            for (let i=7;i<sorok.length;i++) {
+                                let adatok=sorok[i].split(';');
+                                for (let j in adatok) {
+                                    adatok[j]=adatok[j].trim();
+                                }
+                                //console.log(adatok.join(","))
+                                if (adatok.length<2) continue;
+                                let a=adatok[1];
+                                let d=a.substr(0,4)+"-"+a.substr(4,2)+"-"+a.substr(6,2)+"T"+a.substr(8,2)+":"+a.substr(10,2)+"Z";
+                                datum.push(d);
+                                akthom.push(Number(adatok[4]));
                             }
-                            //console.log(adatok.join(","))
-                            if (adatok.length<2) continue;
-                            let a=adatok[1];
-                            let d=a.substr(0,4)+"-"+a.substr(4,2)+"-"+a.substr(6,2)+"T"+a.substr(8,2)+":"+a.substr(10,2)+"Z";
-                            datum.push(d);
-                            akthom.push(Number(adatok[4]));
-                        }
-                        for (let i=1; i<akthom.length-1; i++)
-                            akthom[i]=(akthom[i-1]+akthom[i]+akthom[i+1])/3;
-                        // Define Data
-                        var data = [{
-                            x: datum,
-                            y: akthom,
-                            mode:"lines",
-                            type:"scatter",
-                            line: {shape: 'spline'},
-                        }];                 
-                        console.log(data);
-                        // Define Layout
-                        var layout = {
-                        xaxis: {title: "Time 10min"},
-                        yaxis: {title: "Temperature in °C"},
-                        title: "13704 Meteogram",
-                        automargin: true,
-                        };
+                            // smooth
+                              for (let i=1; i<akthom.length-1; i++)
+                                  akthoms[i]=(akthom[i-1]+akthom[i]+akthom[i+1])/3;
+                            for (let i=0;i<tooltip.length;i++){
+                            if (Number(tooltip[i].substr(0,5)) == code) {
+                                var name = tooltip[i];
+                            }};
 
-                        var config = {responsive: true}
+                            // Define Data
+                            var data1 = {
+                                x: datum,
+                                y: akthom,
+                                mode:"lines",
+                                type:"scatter",
+                                line: {shape: 'spline', color:'rgb(142, 124, 195)', width:3},
+                                name: 'Original'
+                            };
+                            
+                            var data2 = {   
+                                x: datum,
+                                y: akthoms,
+                                mode:"lines",
+                                type:"scatter",
+                                line: {shape: 'spline', color:'rgb(234, 153, 153)', width: 2},
+                                name: 'Smooth'
+                            };
+                            
+                            var data = [data1, data2];
 
-                        Plotly.newPlot("myPlot", data, layout, config);
-                    });
-console.log(datum);                    
+                            // Define Layout
+                            var layout = {
+                            hovermode:'closest',    
+                            xaxis: {title: "Time 10min"},
+                            yaxis: {title: "Temperature in °C"},
+                            height: 500,
+                            title: name,
+                            automargin: true,
+                            annotations: [{
+                                text: akthom[akthom.length-1] + ' °C',
+                                  font: {
+                                  size: 15,
+                                  color: 'rgb(116, 101, 130)',
+                                },
+                                showarrow: false,
+                                align: 'left',
+                                x: 1,
+                                y: 1,
+                                xref: 'paper',
+                                yref: 'paper',
+                              }]
+                            };
 
+                            var config = {responsive: true}
+
+                            Plotly.newPlot("myPlot", data, layout, config);
+
+                        }); 
+        document.getElementById("code").value="";                           
+        };
+
+
+//Press Enter hely kereses    
+var input = document.getElementById("code");
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("Btn1").click();
+        event.currentTarget.value = "";
+    }   
+});  
+
+document.getElementById("Btn1").click();
